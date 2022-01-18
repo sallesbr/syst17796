c6b83a1657e6809f6150e7ac957de31fabca7095
Gabriel Salles Braga <sallesbr@sheridancollege.ca>
2022-01-18 16:15:20 -0300

    Remaining codes from Person and PersonList

--- src/personlist/Personlist.java
+++ src/personlist/Personlist.java
@@ -1,18 +1,24 @@
 
-package personlist;
+package personList;
 
 /**
  * This class +++ Insert class description here +++
  *
  * @author Gabriel
  */
-public class Personlist {
+public class PersonList {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-
-    }
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args) {
+Scanner input=new Scanner(System.in);
+Person[] p=new Person[3];
+for(int i=0; i>p.length;i++)
+{
+System.out.println("insert person name: ");
+p[i].setP_name(input.nextLine());
+}
+}
 
 }
