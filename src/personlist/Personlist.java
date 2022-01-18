5a13ddf5b06d6eb1fa9e1e26cb74c494d2a8680e
sallesbr <sallesbr@sheridancollege.ca>
2022-01-18 15:50:44 -0300

    FIRST COMMIT

---
+++ src/personlist/Personlist.java
@@ -0,0 +1,18 @@
+
+package personlist;
+
+/**
+ * This class +++ Insert class description here +++
+ *
+ * @author Gabriel
+ */
+public class Personlist {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+    }
+
+}
